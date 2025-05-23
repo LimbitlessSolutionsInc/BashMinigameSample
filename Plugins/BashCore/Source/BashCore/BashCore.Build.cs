7fc9364f1ba3e9c9a8fcb4f189bcc25f4104da34
mob205 <mobslayer205@gmail.com>
2025-05-23 16:01:57 -0400

    Fix plugins

---
+++ Plugins/BashCore/Source/BashCore/BashCore.Build.cs
@@ -0,0 +1,30 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class BashCore : ModuleRules
+{
+	public BashCore(ReadOnlyTargetRules Target) : base(Target)
+	{
+		bUsePrecompiled = true;
+				
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core", "LimbitlessBluetoothPlugin",
+				"UMG", "InputCore", "EnhancedInput"
+			}
+		);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+			}
+		);
+	}
+}
